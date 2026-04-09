eba6aafd11674c9646187d877a18955dd9a478ff
mccraigmccraig <mccraigmccraig@gmail.com>
2026-04-09 07:33:19 +0100

    fix dialyzer

--- mix.exs
+++ mix.exs
@@ -15,6 +15,7 @@ defmodule HexPort.MixProject do
       package: package(),
       source_url: "https://github.com/mccraigmccraig/hex_port",
       homepage_url: "https://github.com/mccraigmccraig/hex_port",
+      dialyzer: [plt_add_apps: [:mix]],
       docs: [
         main: "readme",
         extras: [
